c062f77f9eb38a4259b5feb8cbfa767a26909898
jojiO <jinyo6@gmail.com>
2020-07-11 13:20:45 +0900

    ajeo;waf

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ var request = new XMLHttpRequest();
 
 // URLを開く
 request.open('GET', "https://script.google.com/macros/s/AKfycbxafbXHnCT-AbarlGxzFO5RWITubteqSWgSq1fea7mlEgHnugVQ/exec", true);
-
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/users/1', true);
 request.responseType = 'json';
 
 request.onload = function () {
